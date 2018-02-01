668e381d013572487c06a363d254cd2ff35482af
nesparza <nesparza@nesparzaMacBookPro.local>
2018-02-01 18:15:11 -0500

    upgrate

--- script.js
+++ script.js
@@ -10,29 +10,34 @@ var keydir = {
 // When the user click play or press any arrow key the snake starts to move
 window.addEventListener('keydown',
     function (e) {
-    keydir.x = e.x;
-    keydir.y = e.y;
-    console.log(keydir);
-    switch(e.which){
-        case 37: 
-            console.log("press left");
-            break;
-        case 38: 
-            console.log("press up");
-            break;
-        case 39: 
-            console.log("press right");
-            break;
-        case 40: 
-            console.log("press down");
-            snake.update();
-            break;
-        default: 
-            animate();
-            break;
-    }
+        keydir.x = e.x;
+        keydir.y = e.y;
+        console.log(keydir);
+        switch (e.which) {
+            case 37:
+                goLeft();
+                break;
+            case 38:
+                console.log("press up");
+                break;
+            case 39:
+                console.log("press right");
+                break;
+            case 40:
+                goDown();
+                break;
+            default:
+                animate();
+                break;
+        }
     });
-
+//Directions
+function goLeft(){
+    console.log('I am in goLeft Function');
+}
+function goDown(){
+    animate();
+}
 //Created Canvas Snake Home
 
 var canvas = document.getElementById('canvas');
@@ -46,25 +51,28 @@ function Snake(x, y, w, h) {
 
     // Draw the snake body
     this.draw = function () {
-        ctx.fillStyle = 'rgb(0, 0, 0)';
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-        ctx.fillStyle = 'rgba(0, 0, 0)';
-        ctx.fillRect(this.x + 12, this.y, this.w, this.h);
-        ctx.fillStyle = 'rgba(0, 0, 0)';
-        ctx.fillRect(this.x + 24, this.y, this.w, this.h);
+        // head
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+        // body
+            ctx.fillStyle = 'rgba(0, 0, 0)';
+            ctx.fillRect(this.x + 12, this.y, this.w, this.h);
+        // tail
+            ctx.fillStyle = 'rgba(0, 0, 0)';
+            ctx.fillRect(this.x + 24, this.y, this.w, this.h);
+
     }
-    
+
     // Update or move snake
     this.update = function () {
         this.x += 1;
         this.draw();
         if (this.x <= canvas.width && this.y >= canvas.height) {
-            console.log('y:'+ this.y);
-            
+            console.log('y:' + this.y);
+
         }
-        
+
     }
-    console.log(canvas.height);
 
 }
 
@@ -75,6 +83,7 @@ function animate() {
     ctx.clearRect(0, 0, innerWidth, innerHeight);
     snake.update();
 }
+
 
 // When the snake finds a wall stop animation and clean all food she had eated and starts again where she was
 // It the user clicks any arrow the snake should follow the diretion
