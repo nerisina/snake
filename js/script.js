10451b101da1fd0e58414ffef44b0dbdb2c8929a
nesparza <nesparza@nesparzaMacBookPro.local>
2018-02-19 17:39:44 -0500

    snake moving

--- js/script.js
+++ js/script.js
@@ -1,102 +1,74 @@
-// Snake Game
-// Author Neri Esparza
-
+// Create Stage
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var size = 10;
-var snake, x, y;
+var x, y, sl;
+var snake = [];
 
-// Creating the snake squares
-function square(x, y) {
+//    snake.push({
+//        x: 0,
+//        y: 0
+//    });
+
+// push objetcs inside
+//var push = function () {
+//    snake.push({
+//        x: x++,
+//        y: y++
+//    })
+//}
+
+// Creating the snake square and lenght
+var square = function (x, y) {
     ctx.fillStyle = 'rgb(214, 55, 5)';
-    ctx.fillRect(x, y, size, size);
+    ctx.fillRect(x * size, y * size, size, size);
     ctx.strokeStyle = 'rgb(0, 255, 255)';
-    ctx.strokeRect(x, y, size, size);
+    ctx.strokeRect(x * size, y * size, size, size);
 }
-
-// Drawing snake
-function drawSnake() {
-    // create array and add x,y positions
-    snake = [];
-    for (var i = 0; i < 4; i++) {
+var drawWorm = function (x,y) {
+    sl = 3;
+    for (var i = 0; i <= sl; i++) {
         snake.push({
             x: i,
             y: 0
         });
     }
+       square(snake[i].x++, snake[i].y);
+    
+}
 
-    snakeSize = snake.length;
-    for (var a = 0; a <= snakeSize; a++) {
-        var p = snake[a];
-        if (typeof p !== 'undefined') {
-            square(p.x * size, p.y * size);
-        }
+var interval = setInterval(function (x, y) {
+    sl = snake.length;
+//     snake.push({
+//            x: snake[snake.length]+1,
+//            y: 0
+//        });
+    console.log(sl);
+    
+    if(sl<4){
+        temp = snake.pop();
+        
     }
-}
-// Create Food 
-function food() {
-    food = ({
-        rx: Math.round(Math.random() * canvas.width),
-        ry: Math.round(Math.random() * canvas.height)
-    });
-
-    //If body and food doesnt have the sale coordonates created food
-    square(food.rx, food.ry);
-}
-
-// Snake eats  
-function sheEats() {
-    //If head touch the food the snake array increase
-}
-
-//Game over
-function gameOver() {
-    //If the snakes touch any wall stage GAME OVER and RESTART
-    //If snake touch her body GAME OVER and RESTART   
-}
-
-// Snake moves
-// If the user move any of the keys arrow is have to move right/left/up/down
-function sneakyMove() {
-    if (move == "up") {
-        y++;
-    } else if (move == "down") {
-        y--;
-    } else if (move == "left") {
-        x--;
-    } else if (move == "right") {
-        x++;
+    else{
+       drawWorm(x,y);
+        
     }
-}
-// Create Controls
+    
+    
+}, 1000);
 
-// When the key is pressed move the snake whatever the direction it is
-window.addEventListener('keydown',
-    function (e) {
-        switch (e.which) {
-            case 37:
-                move = "left";
-                console.log(move);
-                break;
-            case 38:
-                move = "up";
-                break;
-            case 39:
-                move = "right"
-                break;
-            case 40:
-                move = "down"
-                break;
-            default:
-                break;
-        }
-    });
 
-// Game starts
+//sl = snake.length;
+//
+//for (var i = 0; i <= sl; i++) {
+//    var sn = snake[i];
+//    if (typeof sn !== 'undefined') {
+//        square(sn.x, sn.y);
+//    }
+//}
 
-function gameStarts() {
-    food();
-    drawSnake();
-}
+//setInterval(function() {
+//                  window.location.reload();
+//                }, 30000);
 
-gameStarts();
+drawWorm();
